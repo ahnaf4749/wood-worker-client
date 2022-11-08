fa05ad8d0878453620edb03950a3535f24085ff1
ahnaf4749 <ahnafsagor12@gmail.com>
2022-11-08 23:57:34 +0600

    footer add

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../components/Pages/Footer/Footer';
 import Header from '../components/Pages/Header/Header';
 
 const main = () => {
@@ -7,6 +8,7 @@ const main = () => {
         <div>
             <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
